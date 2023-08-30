aeed385e99b8d53c16aac7f5f5bc3ffff1c327c0
Lucas Ciminelli <lucasaciminelli@gmail.com>
2023-08-30 13:38:57 -0300

    solucionando cositas

--- src/CartManager.js
+++ src/CartManager.js
@@ -1,24 +1,30 @@
 import fs from "fs";
 
 class CartManager {
-  carts;
-  static id;
-
-  constructor() {
-    this.carts = [];
-    this.path = "./data/carts.json";
-
-    if (fs.existsSync(this.path)) {
-      const content = fs.readFileSync(this.path, "utf-8");
-      const carts = JSON.parse(content);
-      if (carts.length > 0) {
-        CartManager.id = carts[carts.length - 1].id + 1;
+    carts;
+    static id;
+  
+    constructor() {
+      this.carts = [];
+      this.path = "./data/carts.json";
+  
+      try {
+        if (fs.existsSync(this.path)) {
+          const content = fs.readFileSync(this.path, "utf-8");
+          this.carts = JSON.parse(content);
+        }
+      } catch (error) {
+        console.log("Error al cargar carritos", error);
+        this.carts = [];
       }
-    } else {
-      CartManager.id = 1;
+  
+      if (this.carts.length > 0) {
+        CartManager.id = this.carts[this.carts.length - 1].id + 1;
+      } else {
+        CartManager.id = 1;
+      }
     }
-  }
-
+  
   generateUniqueId() {
     return CartManager.id++;
   }
@@ -57,7 +63,7 @@ class CartManager {
   }
 
   async addProductToCart(cartId, prodId, quantity) {
-    const foundCart = this.getCartsById(cartId);
+    const foundCart = await this.getCartsById(cartId);
 
     if (!foundCart) {
       console.error("Error, carrito no encontrado");
@@ -79,5 +85,33 @@ class CartManager {
     return foundCart;
   }
 }
+
+async function testCartManager() {
+  
+  const cartManager = new CartManager();
+  const newCart = await cartManager.createCart();
+
+  
+  const productId = 1;
+  const quantity = 2;
+  const updatedCart = await cartManager.addProductToCart(
+    newCart.id,
+    productId,
+    quantity
+  );
+
+
+  if (updatedCart) {
+    console.log("Producto agregado correctamente al carrito:", updatedCart);
+  } else {
+    console.log("Error al agregar producto al carrito");
+  }
+
+
+  const carts = await cartManager.getCarts();
+  console.log("Carritos actualizados:", carts);
+}
+
+//testCartManager();
 
 export default CartManager;
