eeefda045ae6f9c71e74ef95499f2df668e560c4
Lucas Ciminelli <lucasaciminelli@gmail.com>
2023-11-02 23:24:18 -0300

    Entregable 7

--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -3,3 +3,85 @@ import CartManager from "../dao/database/cartManager.js";
 
 const productManager = new ProductManager();
 const cartManager = new CartManager();
+
+export const renderHome = async (req, res) => {
+  const products = await productManager.getProducts();
+  res.render("home", { products });
+};
+
+export const RenderRealTimeProducts = async (req, res) => {
+  res.render("realTimeProducts", {});
+};
+
+export const renderChat = (req, res) => {
+  res.render("chat", {});
+};
+
+export const renderProducts = async (req, res) => {
+  try {
+    const options = {
+      page: req.query.page || 1,
+      limit: req.query.limit || 10,
+      sort: req.query.sort || {},
+    };
+
+    const { first_name, last_name, email, age } = req.session;
+
+    const products = await productManager.getProducts({}, options);
+
+    res.render("products", { first_name, last_name, products });
+  } catch (error) {
+    console.error("Error al obtener y renderizar productos:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+};
+
+export const renderCart = async (req, res) => {
+  const cid = req.params.cid;
+
+  try {
+    const cart = await cartManager.getCartsById(cid);
+
+    if (!cart) {
+      res.status(404).send("Carrito no encontrado");
+      return;
+    }
+
+    const cartProducts = cart.products.map((prod) => ({
+      title: prod.product.title,
+      quantity: prod.quantity,
+    }));
+
+    res.render("cart", { products: cartProducts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
+  }
+};
+
+export const renderLogin = (req, res) => {
+  res.render("login");
+};
+
+export const renderSignup = (req, res) => {
+  res.render("signup");
+};
+
+export const renderProfile = (req, res) => {
+  const { first_name, last_name, email, age, rol } = req.session;
+
+  res.render("profile", { first_name, last_name, email, age, rol });
+};
+
+export const destroySession = (req, res) => {
+  req.session.destroy();
+  res.redirect("/login");
+};
+
+export const renderRecover = (req, res) => {
+  res.render("recover");
+};
+
+export const renderLoginJwt = (req, res) => {
+  res.render("loginJwt");
+};
--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -1,94 +1,32 @@
 import { Router } from "express";
-import ProductManager from "../dao/database/productManager.js";
-import CartManager from "../dao/database/cartManager.js";
 import publicRoutes from "../middlewares/publicRoutes.js";
 import privateRoutes from "../middlewares/privateRoutes.js";
+import {
+  renderHome,
+  RenderRealTimeProducts,
+  renderChat,
+  renderProducts,
+  renderCart,
+  renderLogin,
+  renderSignup,
+  renderProfile,
+  destroySession,
+  renderRecover,
+  renderLoginJwt,
+} from "../controllers/views.controller.js";
 
-
-const productManager = new ProductManager();
-const cartManager = new CartManager();
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const products = await productManager.getProducts();
-  res.render("home", { products });
-});
-
-router.get("/realtimeproducts", async (req, res) => {
-  res.render("realTimeProducts", {});
-});
-
-router.get("/chat", (req, res) => {
-  res.render("chat", {});
-});
-
-router.get("/products", privateRoutes, async (req, res) => {
-  try {
-    const options = {
-      page: req.query.page || 1,
-      limit: req.query.limit || 10,
-      sort: req.query.sort || {},
-    };
-
-    const { first_name, last_name, email, age } = req.session;
-
-    const products = await productManager.getProducts({}, options);
-
-    res.render("products", { first_name, last_name, products });
-  } catch (error) {
-    console.error("Error al obtener y renderizar productos:", error);
-    res.status(500).send("Error interno del servidor");
-  }
-});
-
-router.get("/carts/:cid", privateRoutes, async (req, res) => {
-  const cid = req.params.cid;
-
-  try {
-    const cart = await cartManager.getCartsById(cid);
-
-    if (!cart) {
-      res.status(404).send("Carrito no encontrado");
-      return;
-    }
-
-    const cartProducts = cart.products.map((prod) => ({
-      title: prod.product.title,
-      quantity: prod.quantity,
-    }));
-
-    res.render("cart", { products: cartProducts });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error interno del servidor");
-  }
-});
-
-router.get("/login", publicRoutes, (req, res) => {
-  res.render("login");
-});
-
-router.get("/signup", publicRoutes, (req, res) => {
-  res.render("signup");
-});
-
-router.get("/profile", privateRoutes, (req, res) => {
-  const { first_name, last_name, email, age, rol } = req.session;
-
-  res.render("profile", { first_name, last_name, email, age, rol });
-});
-
-router.get("/logout", (req, res) => {
-  req.session.destroy();
-  res.redirect("/login");
-});
-
-router.get("/recover", publicRoutes, (req, res) => {
-  res.render("recover");
-});
-
-router.get("/loginJwt", publicRoutes, (req, res) => {
-  res.render("loginJwt");
-});
+router.get("/", renderHome);
+router.get("/realtimeproducts", RenderRealTimeProducts);
+router.get("/chat", renderChat);
+router.get("/products", privateRoutes, renderProducts);
+router.get("/carts/:cid", privateRoutes, renderCart);
+router.get("/login", publicRoutes, renderLogin);
+router.get("/signup", publicRoutes, renderSignup);
+router.get("/profile", privateRoutes, renderProfile);
+router.get("/logout", destroySession);
+router.get("/recover", publicRoutes, renderRecover);
+router.get("/loginJwt", publicRoutes, renderLoginJwt);
 
 export default router;
